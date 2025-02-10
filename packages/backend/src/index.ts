cde542b691f726c44761743f532c3c7aa8ac1dbe
Mariano-SI <marianosilva.dev@gmail.com>
2025-02-10 11:06:00 -0300

    feat: estrutura inicial da nova custom action para gerenciar branchs na azure

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -54,4 +54,5 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
 
+backend.add(import('backstage-plugin-scaffolder-backend-module-azure-branch-creator'));
 backend.start();

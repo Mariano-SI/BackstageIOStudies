d27fd923e5609d5c4b597e63e29c065c0d085633
Mariano-SI <marianosilva.dev@gmail.com>
2025-02-11 10:00:42 -0300

    test: testes unitarios da custom action de criar repositorios

--- plugins/scaffolder-backend-module-azure-branch-creator/src/actions/azureBranchCreator.test.ts
+++ plugins/scaffolder-backend-module-azure-branch-creator/src/actions/azureBranchCreator.test.ts
@@ -1,24 +1,142 @@
-import { createExampleAction } from './azureBranchCreator';
-import {createMockActionContext} from '@backstage/plugin-scaffolder-node-test-utils'
+import { createAzureBranchesAction } from './azureBranchCreator';
+import { createMockActionContext } from '@backstage/plugin-scaffolder-node-test-utils';
+import api from '../api/api';  
 
-describe('createExampleAction', () => {
-  it('should call action', async () => {
-    const action = createExampleAction();
+jest.mock('../api/api'); 
 
-    await expect(action.handler(createMockActionContext({
-      input: {
-        myParameter: 'test',
-      },
-    }))).resolves.toBeUndefined()
+describe('createAzureBranchesAction', () => {
+  const mockApiPost = jest.fn();
+  const mockApiGet = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    api.post = mockApiPost;
+    api.get = mockApiGet;
   });
 
-  it('should fail when passing foo', async () => {
-    const action = createExampleAction();
+  it('should create multiple branches successfully', async () => {
+    const action = createAzureBranchesAction();
 
-    await expect(action.handler(createMockActionContext({
-      input: {
-        myParameter: 'foo',
+    mockApiGet.mockResolvedValueOnce({
+      data: {
+        value: [{ objectId: '4faff85b-4be4-4c85-8bbc-d9573831e35a' }],
       },
-    }))).rejects.toThrow("myParameter cannot be 'foo'")
+    });
+
+    const mockContext = createMockActionContext({
+      input: {
+        organization: 'organization',
+        project: 'project',
+        repository: 'repository',
+        branchNames: ['develop', 'qa', 'uat'],
+      },
+    });
+
+    await action.handler(mockContext);
+
+    expect(mockApiGet).toHaveBeenCalledWith(
+      '/organization/project/_apis/git/repositories/repository/refs?api-version=7.1',
+      expect.anything()
+    );
+
+    expect(mockApiPost).toHaveBeenCalledWith(
+      '/organization/project/_apis/git/repositories/repository/refs?api-version=7.1',
+      expect.arrayContaining([
+        expect.objectContaining({
+          name: 'refs/heads/develop',
+          oldObjectId: '0000000000000000000000000000000000000000',
+          newObjectId: '4faff85b-4be4-4c85-8bbc-d9573831e35a',
+        }),
+        expect.objectContaining({
+          name: 'refs/heads/qa',
+          oldObjectId: '0000000000000000000000000000000000000000',
+          newObjectId: '4faff85b-4be4-4c85-8bbc-d9573831e35a',
+        }),
+        expect.objectContaining({
+          name: 'refs/heads/uat',
+          oldObjectId: '0000000000000000000000000000000000000000',
+          newObjectId: '4faff85b-4be4-4c85-8bbc-d9573831e35a',
+        }),
+      ])
+    );
+    expect(mockApiGet).toHaveBeenCalledTimes(1);
+    expect(mockApiPost).toHaveBeenCalledTimes(1);
+  });
+
+  it('should throw an error if no organization provided', async () => {
+    const action = createAzureBranchesAction();
+
+    mockApiGet.mockResolvedValueOnce({
+      data: {
+        value: [{ objectId: '4faff85b-4be4-4c85-8bbc-d9573831e35a' }],
+      },
+    });
+
+    const mockContext = createMockActionContext({
+      input: {
+        project: 'project',
+        repository: 'repository',
+        branchNames: ['develop', 'qa', 'uat'],
+      },
+    });
+
+    await expect(action.handler(mockContext)).rejects.toThrow('Missing required fields');
+  });
+  it('should throw an error if no project provided', async () => {
+    const action = createAzureBranchesAction();
+
+    mockApiGet.mockResolvedValueOnce({
+      data: {
+        value: [{ objectId: '4faff85b-4be4-4c85-8bbc-d9573831e35a' }],
+      },
+    });
+
+    const mockContext = createMockActionContext({
+      input: {
+        organization: 'organization',
+        repository: 'repository',
+        branchNames: ['develop', 'qa', 'uat'],
+      },
+    });
+
+    await expect(action.handler(mockContext)).rejects.toThrow('Missing required fields');
+  });
+  it('should throw an error if no repository provided', async () => {
+    const action = createAzureBranchesAction();
+
+    mockApiGet.mockResolvedValueOnce({
+      data: {
+        value: [{ objectId: '4faff85b-4be4-4c85-8bbc-d9573831e35a' }],
+      },
+    });
+
+    const mockContext = createMockActionContext({
+      input: {
+        organization: 'organization',
+        project: 'project',
+        branchNames: ['develop', 'qa', 'uat'],
+      },
+    });
+
+    await expect(action.handler(mockContext)).rejects.toThrow('Missing required fields');
+  });
+  it('should throw an error if no branchNames provided', async () => {
+    const action = createAzureBranchesAction();
+
+    mockApiGet.mockResolvedValueOnce({
+      data: {
+        value: [{ objectId: '4faff85b-4be4-4c85-8bbc-d9573831e35a' }],
+      },
+    });
+
+    const mockContext = createMockActionContext({
+      input: {
+        organization: 'organization',
+        project: 'project',
+        repository: 'repository',
+      },
+    });
+
+    await expect(action.handler(mockContext)).rejects.toThrow('Missing required fields');
   });
 });

76bab715521e63f6828b98b1b4e113198001e2c9
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-02-17 12:53:02 -0300

    fix: code-review

--- plugins/scaffolder-backend-module-azure-branch-creator/src/api/api.ts
+++ plugins/scaffolder-backend-module-azure-branch-creator/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://dev.azure.com', 
+  baseURL: process.env.AZURE_DEVOPS_API_BASE_URL, 
   headers: {
     'Authorization': `Basic ${Buffer.from(`:${process.env.AZURE_DEVOPS_PAT}`).toString('base64')}`,
     'Content-Type': 'application/json',
